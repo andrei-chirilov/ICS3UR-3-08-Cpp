b9a0d76723c2ad06b43f68acd5f4418d9e3dd074
Ubuntu <ubuntu@ip-172-31-28-78.ec2.internal>
2019-10-20 22:17:35 +0000

    commit

---
+++ leap_year.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2019 Andrei Chirilov All rights reserved.
+//
+// Created by: Andrei Chirilov
+// Created on: October 2019
+// This program does nested if statements
+
+#include <iostream>
+#include <string>
+
+int main() {
+    // variables
+    std::string year;
+    int number;
+
+
+    std::cout << "Enter a year: ";
+    std::cin >> year;
+
+    // process & output
+    try {
+        number = std::stoi(year);
+        if (number % 4 == 0) {
+            if (number % 100 == 0) {
+                if (number % 400 == 0)
+                    std::cout << year << " is a leap year.";
+                else
+                    std::cout << year << " is not a leap year.";
+            } else {
+                std::cout << year << " is a leap year.";
+            }
+        } else {
+            std::cout << year << " is not a leap year.";
+        }
+    }
+    catch (std::invalid_argument) {
+        std::cout << "This wasn't a valid entry, please try again";
+    }
+}
